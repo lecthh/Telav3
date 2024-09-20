90a34e5a5e43e27d36e5469d9b6e7332c859a3f2
lecthh <dev.lecth@gmail.com>
2024-09-20 23:21:48 +0800

    footer and hero

---
+++ resources/views/livewire/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="flex gap-x-[10px] px-[100px] py-6 bg-cNot-black text-white justify-start">
+    <div class="flex flex-col gap-y-[10px] text-sm">
+        <h1 class="font-inter font-bold">Site Links</h1>
+        <ul class="flex gap-x-6">
+            <li class=""><a href="">Home</a></li>
+            <li class=""><a href="">Start Your Custom Order</a></li>
+            <li class=""><a href="">Browse Production Services</a></li>
+            <li class=""><a href="">Become a Partner</a></li>
+        </ul>
+    </div>
+</footer>
