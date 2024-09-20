368726672b59ebb6cb0660a25027e895d369a9d3
Cleverson Sucalit <cleversonsucalit@gmail.com>
2024-09-21 01:32:42 +0800

    lacking footer

--- resources/views/cart/cart.blade.php
+++ resources/views/cart/cart.blade.php
@@ -30,35 +30,35 @@
             
             <div class="flex flex-col gap-y-2">
                 <div class="flex flex-col gap-y-8 p-y-6">
-                    <div class="flex flex-row gap-y-8">
-                        <div class="flex flex-col gap-y-4 px-5 py-5 border border-gray-300">
+                    <div class="flex flex-row gap-x-8 ">
+                        <div class="flex flex-col gap-y-4 px-5 py-5 border border-black rounded-lg bg-[#F3F3F3]">
                             <div class="flex flex-col gap-y-2 5 p-x-2.5 p-y-2.5">
                                 <img src="{{ asset('images/shirt.png') }}" alt="Example Image" class="w-full h-auto">
                             </div>
                         </div>
-                        <div class="flex flex-col gap-y-2">
+                        <div class="flex flex-col gap-y-2 flex-grow">
                             <div class="flex flex-row justify-between">
-                                <h3>Apparel Selected:</h3>
-                                <h3>Jersey</h3>                                
+                                <h3 class="font-inter text-lg">Apparel Selected:</h3>
+                                <h3 class="font-inter font-bold text-lg">Jersey</h3>                                
                             </div>
                             <div class="flex flex-row justify-between">
-                                <h3>Production Type:</h3>
-                                <h3>Sublimation</h3>                                
+                                <h3 class="font-inter text-lg">Production Type:</h3>
+                                <h3 class="font-inter font-bold text-lg">Sublimation</h3>                                
                             </div>
                             <div class="flex flex-row justify-between">
-                                <h3>Order Type:</h3>
-                                <h3>Bulk</h3>                                
+                                <h3 class="font-inter text-lg">Order Type:</h3>
+                                <h3 class="font-inter font-bold text-lg">Bulk</h3>                                
                             </div>
                             <div class="flex flex-row justify-between">
-                                <h3>Customization:</h3>
-                                <h3>Personalized</h3>                                
+                                <h3 class="font-inter text-lg">Customization:</h3>
+                                <h3 class="font-inter font-bold text-lg">Personalized</h3>                                
                             </div>                                                        
                         </div>
                     </div>
-                    <div class="flex flex-col gap-y-1 ml-auto">
-                        <h3>4996 PHP</h3>
+                    <div class="flex flex-col gap-y-1 ml-auto items-end">
+                        <h3 class="font-gilroy font-bold text-2xl text-cPrimary">4996 PHP</h3>
                         <div class="flex flex-row gap-y-2 5">
-                            <h3>remove</h3>
+                            <h3 class="font-gilroy font-bold text-base text-cAccent">remove</h3>
                         </div>
                     </div>                                          
                 </div>
@@ -67,42 +67,54 @@
           
             <div class="flex flex-col gap-y-2">
                 <div class="flex flex-col gap-y-8 p-y-6">
-                    <div class="flex flex-row gap-y-8">
-                        <div class="flex flex-col gap-y-4 px-5 py-5 border border-gray-300">
+                    <div class="flex flex-row gap-x-8 ">
+                        <div class="flex flex-col gap-y-4 px-5 py-5 border border-black rounded-lg bg-[#F3F3F3]">
                             <div class="flex flex-col gap-y-2 5 p-x-2.5 p-y-2.5">
                                 <img src="{{ asset('images/shirt.png') }}" alt="Example Image" class="w-full h-auto">
                             </div>
                         </div>
-                        <div class="flex flex-col gap-y-2">
+                        <div class="flex flex-col gap-y-2 flex-grow">
                             <div class="flex flex-row justify-between">
-                                <h3>Apparel Selected:</h3>
-                                <h3>Jersey</h3>                                
+                                <h3 class="font-inter text-lg">Apparel Selected:</h3>
+                                <h3 class="font-inter font-bold text-lg">Jersey</h3>                                
                             </div>
                             <div class="flex flex-row justify-between">
-                                <h3>Production Type:</h3>
-                                <h3>Sublimation</h3>                                
+                                <h3 class="font-inter text-lg">Production Type:</h3>
+                                <h3 class="font-inter font-bold text-lg">Sublimation</h3>                                
                             </div>
                             <div class="flex flex-row justify-between">
-                                <h3>Order Type:</h3>
-                                <h3>Bulk</h3>                                
+                                <h3 class="font-inter text-lg">Order Type:</h3>
+                                <h3 class="font-inter font-bold text-lg">Bulk</h3>                                
                             </div>
                             <div class="flex flex-row justify-between">
-                                <h3>Customization:</h3>
-                                <h3>Personalized</h3>                                
+                                <h3 class="font-inter text-lg">Customization:</h3>
+                                <h3 class="font-inter font-bold text-lg">Personalized</h3>                                
                             </div>                                                        
                         </div>
                     </div>
-                    <div class="flex flex-col gap-y-1 ml-auto">
-                        <h3>4996 PHP</h3>
+                    <div class="flex flex-col gap-y-1 ml-auto items-end">
+                        <h3 class="font-gilroy font-bold text-2xl text-cPrimary">4996 PHP</h3>
                         <div class="flex flex-row gap-y-2 5">
-                            <h3>remove</h3>
+                            <h3 class="font-gilroy font-bold text-base text-cAccent">remove</h3>
                         </div>
                     </div>                                          
                 </div>
                 <hr>
             </div>              
 
-        </div>  
+        </div> 
+        
+        <div class="flex flex-row justify-between">
+            <h3 class="font-gilroy font-bold text-[30px]">Total</h3>
+            <h3 class="font-gilroy font-bold text-[30px]">10000 PHP</h3>
+        </div>
+
+        <div class="flex flex-col gap-y-2.5">
+            <div class="flex flex-col gap-y-2.5 py-3.5 items-start">
+                @livewire('button', ['text' => 'Checkout'])
+            </div>
+        </div>
+
     </div>
 </body>
 
