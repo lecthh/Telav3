5a8a5f1656d4ebaeb99f17412e57ba9fd9fa7196
Cleverson Sucalit <cleversonsucalit@gmail.com>
2024-09-21 00:53:35 +0800

    rough look 2

--- resources/views/cart/cart.blade.php
+++ resources/views/cart/cart.blade.php
@@ -64,39 +64,44 @@
                 </div>
                 <hr>
             </div>  
-            <div class="flex flex-col gap-y-8 p-y-6">
-                <div class="flex flex-row gap-y-8">
-                    <div class="flex flex-col gap-y-4 px-5 py-5 border border-gray-300">
-                        <div class="flex flex-col gap-y-2 5 p-x-2.5 p-y-2.5">
-                            <img src="{{ asset('images/shirt.png') }}" alt="Example Image" class="w-full h-auto">
+          
+            <div class="flex flex-col gap-y-2">
+                <div class="flex flex-col gap-y-8 p-y-6">
+                    <div class="flex flex-row gap-y-8">
+                        <div class="flex flex-col gap-y-4 px-5 py-5 border border-gray-300">
+                            <div class="flex flex-col gap-y-2 5 p-x-2.5 p-y-2.5">
+                                <img src="{{ asset('images/shirt.png') }}" alt="Example Image" class="w-full h-auto">
+                            </div>
+                        </div>
+                        <div class="flex flex-col gap-y-2">
+                            <div class="flex flex-row justify-between">
+                                <h3>Apparel Selected:</h3>
+                                <h3>Jersey</h3>                                
+                            </div>
+                            <div class="flex flex-row justify-between">
+                                <h3>Production Type:</h3>
+                                <h3>Sublimation</h3>                                
+                            </div>
+                            <div class="flex flex-row justify-between">
+                                <h3>Order Type:</h3>
+                                <h3>Bulk</h3>                                
+                            </div>
+                            <div class="flex flex-row justify-between">
+                                <h3>Customization:</h3>
+                                <h3>Personalized</h3>                                
+                            </div>                                                        
                         </div>
                     </div>
-                    <div class="flex flex-col gap-y-2">
-                        <div class="flex flex-row justify-between">
-                            <h3>Apparel Selected:</h3>
-                            <h3>Jersey</h3>                                
+                    <div class="flex flex-col gap-y-1 ml-auto">
+                        <h3>4996 PHP</h3>
+                        <div class="flex flex-row gap-y-2 5">
+                            <h3>remove</h3>
                         </div>
-                        <div class="flex flex-row justify-between">
-                            <h3>Production Type:</h3>
-                            <h3>Sublimation</h3>                                
-                        </div>
-                        <div class="flex flex-row justify-between">
-                            <h3>Order Type:</h3>
-                            <h3>Bulk</h3>                                
-                        </div>
-                        <div class="flex flex-row justify-between">
-                            <h3>Customization:</h3>
-                            <h3>Personalized</h3>                                
-                        </div>                                                        
-                    </div>
+                    </div>                                          
                 </div>
-                <div class="flex flex-col gap-y-1 ml-auto">
-                    <h3>4996 PHP</h3>
-                    <div class="flex flex-row gap-y-2 5">
-                        <h3>remove</h3>
-                    </div>
-                </div>                                          
-            </div>            
+                <hr>
+            </div>              
+
         </div>  
     </div>
 </body>
