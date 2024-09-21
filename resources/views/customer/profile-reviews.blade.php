2cb790b75c64108e8b33e77a8362708ced227bd5
Cleverson Sucalit <cleversonsucalit@gmail.com>
2024-09-22 06:58:03 +0800

    static reviews done

--- resources/views/customer/profile-reviews.blade.php
+++ resources/views/customer/profile-reviews.blade.php
@@ -12,55 +12,84 @@
 
 <body>
     @livewire('navigation-bar')
-    <div class="flex flex-col gap-y-[60px] px-[200px] py-[100px]">
-        <div class="flex flex-col gap-y-5">
-            <div class="flex flex-col gap-y-3">
-                <h1 class="font-gilroy font-bold text-5xl">Profile Page</h1>
+    <div class="flex flex-row">
+        <div class="flex flex-col gap-y-[60px] px-[200px] py-[100px] w-full">
+
+            <div class="flex flex-col gap-y-5">
+                <div class="flex flex-col gap-y-3">
+                    <h1 class="font-gilroy font-bold text-5xl">Profile Page</h1>
+                </div>
+                <div class="flex flex-row gap-x-5 ">
+                    <div class="flex flex-row gap-x-[60px] py-3">
+                        <h1 class="font-inter font-bold text-2xl">Basics</h1>
+                    </div>
+                    <div class="flex flex-row gap-x-[60px] py-3">
+                        <h1 class="font-inter font-bold text-2xl">Orders</h1>
+                    </div>
+                    <div class="flex flex-row gap-x-[60px] py-3">
+                        <h1 class="font-inter font-bold text-2xl  text-cPrimary">Reviews</h1>
+                    </div>                                
+                </div>                    
             </div>
 
-            <!-- GAP-X-5 NOT GIVING SPACING FOR SOME REASON -->
-            <div class="flex flex-row gap-x-5 ">
-                <div class="flex flex-row gap-x-[60px] py-3">
-                    <h1 class="font-inter font-bold text-2xl">Basics</h1>
+            <div class="flex flex-col">
+                <div class="flex flex-row gap-x-[18px]">
+                    <div class="flex flex-col gap-y-4 flex-grow">
+                        <div class="flex flex-row items-start justify-between">
+                            <div class="flex flex-col gap-y-2">
+                                <div class="flex flex-col">
+                                    <h2 class="font-gilroy font-bold text-lg">EchoPoint Productions</h2>
+                                </div>
+                                <div class="flex flex-row gap-x-2">
+                                    <a href="">@include('svgs.star')</a>
+                                    <a href="">@include('svgs.star')</a>
+                                    <a href="">@include('svgs.star')</a>
+                                    <a href="">@include('svgs.star')</a>
+                                    <a href="">@include('svgs.star')</a>
+                                </div>
+                            </div>
+                            <h2 class="font-gilroy font-bold text-base text-gray-500">September 13, 2024</h2>
+                        </div>
+                        <h2 class="font-inter text-sm text-gray-500">I like the quality of the cloth. Also, the sewing.</h2>
+                        <div class="flex flex-row gap-x-3">
+                            <div class="flex flex-row gap-x-2.5 bg-[#D69FFB]/20 p-4 rounded-lg w-[80px] h-[80px]">
+                                <!-- Your content here -->
+                            </div>
+                            <div class="flex flex-row gap-x-2.5 bg-[#D69FFB]/20 p-4 rounded-lg w-[80px] h-[80px]">
+                                <!-- Your content here -->
+                            </div>                            
+                        </div>
+                        <hr>
+                    </div>
                 </div>
-                <div class="flex flex-row gap-x-[60px] py-3">
-                    <h1 class="font-inter font-bold text-2xl">Orders</h1>
-                </div>
-                <div class="flex flex-row gap-x-[60px] py-3">
-                    <h1 class="font-inter font-bold text-2xl text-cPrimary">Reviews</h1>
-                </div>                                
-            </div>
 
-            <div class="flex flex-col gap-y-6 w-[600px]">
-                <div class="flex flex-col gap-y-4">
-                    <h2 class="font-inter font-bold text-lg">Display Name</h2>
-                    <div class="flex flex-row gap-x-2.5 px-5 py-4 border border-black rounded-lg">
-                        <h3 class="font-inter text-base">Jane</h3>
+                <div class="flex flex-row gap-x-[18px]">
+                    <div class="flex flex-col gap-y-4 w-full flex-grow">
+                        <div class="flex flex-row items-start justify-between">
+                            <div class="flex flex-col gap-y-2">
+                                <div class="flex flex-col">
+                                    <h2 class="font-gilroy font-bold text-lg">EchoPoint Productions</h2>
+                                </div>
+                                <div class="flex flex-row gap-x-2">
+                                    <a href="">@include('svgs.star')</a>
+                                    <a href="">@include('svgs.star')</a>
+                                    <a href="">@include('svgs.star')</a>
+                                    <a href="">@include('svgs.star')</a>
+                                    <a href="">@include('svgs.star')</a>
+                                </div>
+                            </div>
+                            <h2 class="font-gilroy font-bold text-base text-gray-500">September 13, 2024</h2>
+                        </div>
+                        <h2 class="font-inter text-sm text-gray-500">I like the quality of the cloth. Also, the sewing.</h2>
+                        <hr>
                     </div>
-                </div>
-                <div class="flex flex-col gap-y-4">
-                    <h2 class="font-inter font-bold text-lg">Email</h2>
-                    <div class="flex flex-row gap-x-2.5 px-5 py-4 border border-black rounded-lg">
-                        <h3 class="font-inter text-base">jane@gmail.com</h3>
-                    </div>
-                </div>
-                <div class="flex flex-col gap-y-4">
-                    <h2 class="font-inter font-bold text-lg">Mobile no.</h2>
-                    <div class="flex flex-row gap-x-2.5 px-5 py-4 border border-black rounded-lg">
-                        <h3 class="font-inter text-base">Jane</h3>
-                    </div>
-                </div>                                
-            </div>
+                </div>                
 
-            <div class="flex flex-row gap-x-3 h-[50px]">
-                <div class="flex flex-col gap-y-2.5">
-                    @livewire('button', ['text' => 'Save'])
-                </div>
             </div>
 
         </div>
 
-        <div class="flex flex-col gap-y-6"></div>
+    </div>
 </body>
 
 
