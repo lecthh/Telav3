5b6ad7b291c6534b58e05889f5cb23ec1bb8bcb8
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2025-03-05 22:19:31 +0800

    feat: implement signup functionality in modal login with toggle option

--- app/Livewire/ModalLogin.php
+++ app/Livewire/ModalLogin.php
@@ -4,11 +4,14 @@ namespace App\Livewire;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Session;
-
+use Livewire\Component;
 use LivewireUI\Modal\ModalComponent;
 
 class ModalLogin extends ModalComponent
 {
+    public $isSignup = false;
+    public $first_name, $last_name, $email, $password, $password_confirmation;
+
     public static function modalMaxWidth(): string
     {
         return 'lg';
@@ -18,6 +21,13 @@ class ModalLogin extends ModalComponent
     {
         Session::put('url.intended', URL::previous());
     }
+
+    public function toggleSignup()
+    {
+        $this->isSignup = !$this->isSignup;
+        $this->resetValidation();
+    }
+
     public function render()
     {
         return view('livewire.modal-login');
--- resources/views/livewire/modal-login.blade.php
+++ resources/views/livewire/modal-login.blade.php
@@ -1,28 +1,59 @@
-<div class="flex flex-col gap-y-[60px] w-fill px-[50px] pt-[52px] pb-[100px] rounded-md bg-white drop-shadow-md justify-center">
+<div class="flex flex-col gap-y-[40px] w-fill px-[50px] pt-[52px] pb-[100px] rounded-md bg-white drop-shadow-md justify-center">
     <div class="flex flex-col gap-y-8">
-        <div class="flex justify-end">@include('svgs.delete')</div>
-        <div class="flex flex-col gap-y-8 justify-center items-center">
+        <div class="flex justify-end cursor-pointer" wire:click="$emit('closeModal')">
+            @include('svgs.delete')
+        </div>
+        <div class="flex flex-col gap-y-1 justify-center items-center">
             @include('svgs.logo')
-            <h1 class="font-gilroy font-bold text-3xl">Log in or Sign Up</h1>
+            <h1 class="font-inter text-lg font-semibold text-Colors/Text/text-primary(900)">
+                {{ $isSignup ? 'Sign Up' : 'Log into your account' }}
+            </h1>
+            <span class="font-inter text-sm font-normal text-Colors/Text/text-tertiary(600) ">Welcome back! Please enter your details</span>
         </div>
     </div>
+
     <div class="flex flex-col gap-y-10 font-inter w-full">
         <div class="flex flex-col gap-y-5">
+            @if($isSignup)
             <div class="flex flex-col gap-y-3">
-                <h4 class="font-bold text-lg">Email</h4>
-                <input type="text" class="border border-black rounded-md px-5 py-4" placeholder="email@gmail.com">
+                <h4 class="font-medium text-sm text-Colors/Text/text-secondary(700)">Name</h4>
+                <input type="text" class="border border-Colors/Border/border-primary rounded-lg px-5 py-4"
+                    placeholder="Enter your first name" wire:model="name">
             </div>
-            <span class="items-center justify-center text-red hidden">Invalid email or password</span>
+            @endif
+
+            <div class="flex flex-col gap-y-3">
+                <h4 class="font-medium text-sm text-Colors/Text/text-secondary(700)">Email</h4>
+                <input type="text" class="border border-Colors/Border/border-primary rounded-lg px-5 py-4"
+                    placeholder="email@gmail.com" wire:model="email">
+            </div>
+
+            <div class="flex flex-col gap-y-3">
+                <h4 class="font-medium text-sm text-Colors/Text/text-secondary(700)">Password</h4>
+                <input type="password" class="border border-Colors/Border/border-primary rounded-lg px-5 py-4"
+                    placeholder="Enter password" wire:model="password">
+            </div>
+            <span class="items-center justify-center text-red hidden">
+                Invalid email or password
+            </span>
         </div>
+
         <div class="flex flex-col gap-y-4 font-inter justify-center items-center">
-            <div class="flex w-full bg-cPrimary rounded-md px-6 py-[14px] text-white text-base items-center justify-center">
-                Continue with Email
-            </div>
+            <button class="flex w-full bg-cPrimary rounded-md px-6 py-[14px] text-white text-base items-center justify-center">
+                {{ $isSignup ? 'Sign Up' : 'Continue with Email' }}
+            </button>
             <span class="text-sm">or</span>
-            <a href="{{ route('google.redirect') }}" class="flex w-full rounded-md px-6 py-[14px] gap-x-3 bg-white border border-black text-black text-base items-center justify-center">
+            <a href="{{ route('google.redirect') }}"
+                class="flex w-full rounded-md px-6 py-[14px] gap-x-3 bg-white border border-black text-black text-base items-center justify-center">
                 @include('svgs.google')
                 Continue with Google
             </a>
+        </div>
+
+        <div class="text-center">
+            <span class="text-sm cursor-pointer text-cPrimary" wire:click="toggleSignup">
+                {{ $isSignup ? 'Already have an account? Log in' : 'No account? Click here to sign up' }}
+            </span>
         </div>
     </div>
 </div>
