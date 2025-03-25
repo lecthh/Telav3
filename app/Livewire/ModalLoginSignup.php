46b7fb2d93e26705f1e757c3c7e5f69186b79b57
lecthh <dev.lecth@gmail.com>
2025-03-25 13:57:04 +0800

    checkout receopts

--- app/Livewire/ModalLoginSignup.php
+++ app/Livewire/ModalLoginSignup.php
@@ -9,18 +9,29 @@ use LivewireUI\Modal\ModalComponent;
 
 class ModalLoginSignup extends ModalComponent
 {
+    public $isSignup = false;
+    public $isForgotPassword = false;
+    public $signupStep = 1;
+    public $name, $email, $password, $password_confirmation;
+    public $rememberMe = false;
+    public $passwordResetStatus = null;
+    public $userEnteredVerificationCode;
+    public $generatedVerificationCode;
+    public $code1, $code2, $code3, $code4, $code5, $code6;
+
     public static function modalMaxWidth(): string
     {
-        return 'sm';
+        return 'lg';
     }
+
 
     public function mount()
     {
         Session::put('url.intended', URL::previous());
-        dd(Session::get('url.intended'));
     }
+    
     public function render()
     {
-        return view('livewire.modal-login-signup');
+        return view('livewire.modal-login');
     }
 }
