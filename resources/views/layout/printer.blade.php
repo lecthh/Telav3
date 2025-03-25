6a0f282c78d8dabc0f54dfadf8214aff42d88fe7
lecthh <dev.lecth@gmail.com>
2025-03-25 15:16:29 +0800

    initial checkout receipt

--- resources/views/layout/printer.blade.php
+++ resources/views/layout/printer.blade.php
@@ -91,6 +91,13 @@
                 <span>Reviews</span>
             </a>
             
+            <a href="{{ route('partner.printer.profile.transactions') }}" class="flex items-center gap-x-3 px-3 py-2.5 rounded-md {{ request()->routeIs('partner.printer.profile.transactions') ? 'bg-cAccent/20 text-cAccent font-medium' : 'text-gray-700 hover:bg-cAccent/10' }} transition-colors">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
+                </svg>
+                <span>Transactions</span>
+            </a>
+            
             <a href="#" class="flex items-center gap-x-3 px-3 py-2.5 rounded-md text-gray-700 hover:bg-cAccent/10 transition-colors">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,8 @@ Route::prefix('partner')->name('partner.')->middleware('ProductionAdminOnly')->g
 
         Route::get('/profile/pricing', [EditProducerAccountController::class, 'index'])->name('profile.pricing');
         Route::get('/profile/reviews', [PrinterDashboardController::class, 'reviews'])->name('profile.reviews');
+        Route::get('/profile/transactions', [PrinterDashboardController::class, 'transactions'])->name('profile.transactions');
+        Route::post('/profile/upload-receipt/{order_id}', [PrinterDashboardController::class, 'uploadBalanceReceipt'])->name('profile.upload-receipt');
         Route::post('/profile/update', [EditProducerAccountController::class, 'update'])->name('profile.update');
         Route::post('/profile/pricing/update', [EditProducerAccountController::class, 'updatePricing'])->name('profile.pricing.update');
         Route::get('/profile/notifications', [PrinterDashboardController::class, 'notifications'])->name('profile.notifications');
@@ -141,6 +143,9 @@ Route::prefix('customer/place-order')->name('customer.place-order.')->group(func
     Route::get('/review/{apparel}/{productionType}/{company}', [OrderController::class, 'review'])->name('review');
     Route::post('/review/{apparel}/{productionType}/{company}', [OrderController::class, 'storeReview'])->name('review-post');
 });
+
+// Receipt confirmation route - accessible by both customers and printers when order_id is provided
+Route::get('/order-receipt', [ConfirmationMessageController::class, 'confirmation'])->name('order.receipt');
 
 Route::middleware(['CustomerOnly'])->group(function () {
     Route::get('/cart', [CartController::class, 'showCart'])->name('customer.cart');
