6fa255b33dbb08fdb9b56d698939685bdd23bfb3
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2025-03-31 23:23:28 +0800

    migration for business documents

---
+++ database/migrations/2025_03_31_151940_bussiness-documents.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBusinessDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('business_documents', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('production_company_id');
+            $table->string('name');
+            $table->string('path');
+            $table->timestamps();
+
+            // Foreign key constraint linking to production_companies table
+            $table->foreign('production_company_id')
+                ->references('id')->on('production_companies')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('business_documents');
+    }
+}
