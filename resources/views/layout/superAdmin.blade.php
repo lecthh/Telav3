032c45d8feb5a14f079fb7a07d69dc574a11d3c8
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2025-04-19 20:17:09 +0800

    feat: Update link in superAdmin layout to remove specific route for user profile

--- resources/views/layout/superAdmin.blade.php
+++ resources/views/layout/superAdmin.blade.php
@@ -16,7 +16,7 @@ $pendingReportsCount = \App\Models\Report::where('status', 'pending')->count();
         <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
       </svg>
     </div>
-    <a href="{{ route('partner.printer.profile.basics') }}" class="font-gilroy font-bold text-base truncate text-gray-900">
+    <a href="" class="font-gilroy font-bold text-base truncate text-gray-900">
       {{ auth()->user()->name }}
     </a>
   </div>
